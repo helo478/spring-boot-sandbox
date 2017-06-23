dac847303265526347da9157a5301b56373b1bfb
Don Subert <helo478@gmail.com>
2017-06-22 22:02:46 -0700

    added logging with logback and slf4j

--- logback-sandbox/src/main/java/com/helo478/springboot/logback/Application.java
+++ logback-sandbox/src/main/java/com/helo478/springboot/logback/Application.java
@@ -2,6 +2,8 @@ package com.helo478.springboot.logback;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,23 +13,26 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+			logger.info("inserting {}", (Object) args);
+			logger.debug("Let's inspect the beans provided by Spring Boot:");
 
-        };
-    }
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				logger.debug(beanName);
+			}
+
+		};
+	}
 
 }
--- logback-sandbox/src/main/java/com/helo478/springboot/logback/HelloController.java
+++ logback-sandbox/src/main/java/com/helo478/springboot/logback/HelloController.java
@@ -1,13 +1,18 @@
 package com.helo478.springboot.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
 	@RequestMapping("/")
 	public String index() {
+		logger.debug("entering index()");
 		return "Greetings from Spring Boot!";
 	}
 
